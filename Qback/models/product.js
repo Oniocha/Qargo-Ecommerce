62bf10bc44545e110c46583f6a32234b4f5203b9
David Quartey <42542676+DavidQuartz@users.noreply.github.com>
2020-10-17 20:05:41 +0000

    👗 Adjusted Product model
    Added Seller to Product Schema

--- Qback/models/product.js
+++ Qback/models/product.js
@@ -32,6 +32,10 @@ const productSchema = new mongoose.Schema(
       type: Number,
       default: 0,
     },
+    seller: {
+      type: String,
+      trim: true,
+    },
     featured: {
       type: Boolean,
       default: false,
