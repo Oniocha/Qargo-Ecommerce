ce43a74c1436b63f1f123b3e7bee16ee40dbdacd
David Quartey <42542676+DavidQuartz@users.noreply.github.com>
2020-10-21 17:35:23 +0000

    👌🏽 Verifying Payment
    Began payment verification process

--- Qback/controllers/raveController.js
+++ Qback/controllers/raveController.js
@@ -1,14 +1,15 @@
 require("dotenv").config();
 const axios = require("axios");
 
-let API_KEY = process.env.RAVE_PULICKEY;
-let payUrl = process.env.RAVE_API;
-let secret = process.env.RAVE_SECRETKEY;
+const API_KEY = process.env.RAVE_PULICKEY,
+  payUrl = process.env.RAVE_API,
+  verifyUrl = process.env.RAVE_VERIFY,
+  secret = process.env.RAVE_SECRETKEY;
 
 exports.initTransaction = (req, res) => {
   let payment = req.body;
   axios({
-    url: payUrl,
+    url: `${payUrl}`,
     method: "POST",
     headers: {
       Authorization: `Bearer ${secret}`,
@@ -16,16 +17,34 @@ exports.initTransaction = (req, res) => {
     data: payment,
   })
     .then((data) => {
-      // res.send(response);
-      // console.log(data.data.status);
       if (data.data.status === "success") {
-        console.log(data);
-        res.json();
+        res.json(data.data);
       } else {
         return res.status(400).json({
-          error: "Payment failed",
+          error: "Failed to generate payment",
         });
       }
     })
-    .catch((err) => console.log(err));
+    .catch((err) =>
+      res.status(400).json({
+        error: err,
+      })
+    );
+};
+
+exports.verifyTransaction = (req, res) => {
+  let transactionId = req.body.transactionId;
+  console.log(transactionId);
+  axios({
+    method: "GET",
+    url: `${verifyUrl}/${transactionId}/verify`,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${secret}`,
+    },
+  })
+    .then((data) => {
+      res.json(data.data);
+    })
+    .catch((err) => res.status(400).json({ error: err }));
 };
--- Qback/routes/raveRoutes.js
+++ Qback/routes/raveRoutes.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-const { userById } = require("../controllers/userController");
-const { requireSignin } = require("../controllers/authController");
-const { initTransaction } = require("../controllers/raveController");
+// const { userById } = require("../controllers/userController");
+// const { requireSignin } = require("../controllers/authController");
 
+const {
+  initTransaction,
+  verifyTransaction,
+} = require("../controllers/raveController");
+
+// Initiate transaction
 router.post("/rave/init", initTransaction);
+
+// Verify transaction
+router.post("/rave/verify", verifyTransaction);
 
 // router.param("userId", userById);
 module.exports = router;
