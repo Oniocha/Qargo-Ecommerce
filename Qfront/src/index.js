4f17511066b2c45c5e68ea8043144fbd20511deb
Abubakar <Haywayaheadshotstrategy.fx@gmail.com>
2023-09-21 10:02:01 -0500

    wrap routes in Provider and supply store

--- Qfront/src/index.js
+++ Qfront/src/index.js
@@ -5,6 +5,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import Routes from "./Routes";
 import * as serviceWorker from "./serviceWorker";
+import {Provider} from "react-redux";
+import store from './redux/configStore';
 
 // console.log(`
 
@@ -32,9 +34,11 @@ import * as serviceWorker from "./serviceWorker";
 // /                           \
 // `);
 
-ReactDOM.render(<Routes />, document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <Provider store={store}>
+      <Routes />
+    </Provider>,
+    document.getElementById("root")
+  );
+  
+  serviceWorker.unregister();
