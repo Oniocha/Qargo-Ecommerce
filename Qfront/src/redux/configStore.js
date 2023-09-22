bdbc2e7eba9c5bafc3aad9046e1ce1a55f8636a8
Abubakar <Haywayaheadshotstrategy.fx@gmail.com>
2023-09-22 10:43:35 -0500

    initialize store and its product selecting vars

--- Qfront/src/redux/configStore.js
+++ Qfront/src/redux/configStore.js
@@ -1,9 +1,13 @@
-import newArrivalsReducer from './product/NewArrivals';
+import LoadByPriceReducer from './product/loadByPrice/LoadByPrice';
+import loadBySellReducer from './product/loadBySell/LoadBySell';
+import newArrivalsReducer from './product/newArrivals/NewArrivals';
 import {configureStore} from '@reduxjs/toolkit';
 
 const store = configureStore({
     reducer: {
-        newArrivals : newArrivalsReducer
+        newArrivals : newArrivalsReducer,
+        loadBySell : loadBySellReducer,
+        loadByPrice : LoadByPriceReducer,
     }
 });
 
