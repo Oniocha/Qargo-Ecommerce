3fff071e0118639b2b42e338e406e5a7d73c1122
Abubakar <Haywayaheadshotstrategy.fx@gmail.com>
2023-09-23 20:02:59 -0500

    delete filteredResults API call

--- Qfront/src/API_CALLS/userApis.js
+++ Qfront/src/API_CALLS/userApis.js
@@ -1,19 +1,5 @@
 import queryString from "query-string";
 
-export const getFilteredProducts = (skip, limit, filters) => {
-  const data = { skip, limit, filters };
-  return fetch(process.env.REACT_APP_API_URL + "/products/by/search", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-type": "application/json",
-    },
-    body: JSON.stringify(data),
-  })
-    .then((res) => res.json())
-    .catch((err) => console.log(err));
-};
-
 // API url for fetching all departments
 let one = process.env.REACT_APP_API_URL + "/departments";
 
