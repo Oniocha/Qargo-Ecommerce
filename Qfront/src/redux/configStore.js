37ffd140361c9c92dbf6fee2cd6fc0ced815b8f9
Abubakar <Haywayaheadshotstrategy.fx@gmail.com>
2023-09-23 20:07:01 -0500

    add filteredProductsReducer to store

--- Qfront/src/redux/configStore.js
+++ Qfront/src/redux/configStore.js
@@ -1,9 +1,11 @@
-import LoadProductsReducer from './product/loadProducts/LoadProducts';
+import loadProductsReducer from './product/loadProducts/LoadProducts';
+import filteredProductsReducer from './product/filteredProducts/FilteredProducts';
 import {configureStore} from '@reduxjs/toolkit';
 
 const store = configureStore({
     reducer: {
-        loadProducts : LoadProductsReducer,
+        loadProducts : loadProductsReducer,
+        loadFilteredProducts: filteredProductsReducer,
     }
 });
 
