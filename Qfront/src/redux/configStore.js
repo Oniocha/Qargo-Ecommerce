5b36cf9c7c11bdb7057a921fa83bb6cfe9fab3d2
Abubakar <Haywayaheadshotstrategy.fx@gmail.com>
2023-09-25 07:08:25 -0500

    add departments reducer

--- Qfront/src/redux/configStore.js
+++ Qfront/src/redux/configStore.js
@@ -1,11 +1,13 @@
 import loadProductsReducer from './product/loadProducts/LoadProducts';
 import filteredProductsReducer from './product/filteredProducts/FilteredProducts';
-import {configureStore} from '@reduxjs/toolkit';
+import departmentsReducer from './departments/Departments';
+import { configureStore } from '@reduxjs/toolkit';
 
 const store = configureStore({
     reducer: {
         loadProducts : loadProductsReducer,
-        loadFilteredProducts: filteredProductsReducer,
+        loadFilteredProducts : filteredProductsReducer,
+        loadDepartments : departmentsReducer
     }
 });
 
