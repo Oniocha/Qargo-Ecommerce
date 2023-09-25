86d800a0a723d43bacc21a979792eb20224a6eeb
Abubakar <Haywayaheadshotstrategy.fx@gmail.com>
2023-09-25 07:07:16 -0500

    move api file to redux folder

--- Qfront/src/redux/api.js
+++ Qfront/src/redux/api.js
@@ -3,3 +3,4 @@ export const getAllCategoriesApi = `${process.env.REACT_APP_API_URL}/categories`
 export const loadBySellApi = `${process.env.REACT_APP_API_URL}/products?sortBy=sold&order=desc&limit=10`;
 export const getNewArrivalsApi = `${process.env.REACT_APP_API_URL}/products?sortBy=sold&order=desc&limit=10`;
 export const fetchFilteredProductsApi = `${process.env.REACT_APP_API_URL}/products/by/search`;
+export const getDepartmentsApi = `${process.env.REACT_APP_API_URL}/departments`;
