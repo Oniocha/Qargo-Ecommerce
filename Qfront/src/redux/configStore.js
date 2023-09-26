af758c55637514bf3a4106d59589fc0fbc73d018
Abubakar <Haywayaheadshotstrategy.fx@gmail.com>
2023-09-26 13:03:59 -0500

    add reducer to load a product

--- Qfront/src/redux/configStore.js
+++ Qfront/src/redux/configStore.js
@@ -1,13 +1,15 @@
 import loadProductsReducer from './product/loadProducts/LoadProducts';
 import filteredProductsReducer from './product/filteredProducts/FilteredProducts';
 import departmentsReducer from './departments/Departments';
+import loadProductReducer from './product/loadProduct/LoadProduct';
 import { configureStore } from '@reduxjs/toolkit';
 
 const store = configureStore({
     reducer: {
         loadProducts : loadProductsReducer,
         loadFilteredProducts : filteredProductsReducer,
-        loadDepartments : departmentsReducer
+        loadDepartments : departmentsReducer,
+        loadProduct : loadProductReducer
     }
 });
 
