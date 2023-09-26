2f86a17bde8a1d654d1135cedac3f2528e7b5ef0
Abubakar <Haywayaheadshotstrategy.fx@gmail.com>
2023-09-26 08:17:52 -0500

    import products api to include required endpoints

--- Qfront/src/redux/product/loadProducts/actions.js
+++ Qfront/src/redux/product/loadProducts/actions.js
@@ -1,11 +1,11 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
-import { getProductsByPriceApi, getAllCategoriesApi,
-    loadBySellApi, getNewArrivalsApi
+import { getAllCategoriesApi, productsEndPointAPi
 } from "../../api";
 
 // Create action to load by price
 export const getProductsByPrice = createAsyncThunk("LOAD_BY_PRICE", async() => {
+    const getProductsByPriceApi = productsEndPointAPi + `?sortBy=price&order=asc&limit=10`;
     const response = await axios.get(getProductsByPriceApi);
     return response.data.data;
 });
@@ -18,12 +18,14 @@ export const getAllCategories = createAsyncThunk("LOAD_BY_CATEGORIES", async() =
 
 // create fetch action for fetch by price
 export const loadBySell = createAsyncThunk("LOAD_BY_SELL", async () => {
+    const loadBySellApi = productsEndPointAPi + `?sortBy=sold&order=desc&limit=10`;
     const response = await axios(loadBySellApi);
     return response.data.data;
 });
 
 // create fetch action for new Arrivals
 export const getNewArrivals = createAsyncThunk("FETCH_PRODUCTS", async () => {
+    const getNewArrivalsApi = productsEndPointAPi + `?sortBy=sold&order=desc&limit=10`;
     const response = await axios(getNewArrivalsApi);
     return response.data.data;
 });
