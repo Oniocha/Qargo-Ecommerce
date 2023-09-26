393dda03b46be3b477b127b4aa98c965634d8c65
Abubakar <Haywayaheadshotstrategy.fx@gmail.com>
2023-09-26 13:03:41 -0500

    include endpoint for a product

--- Qfront/src/redux/api.js
+++ Qfront/src/redux/api.js
@@ -1,4 +1,4 @@
 export const getAllCategoriesApi = `${process.env.REACT_APP_API_URL}/categories`;
-export const fetchFilteredProductsApi = `${process.env.REACT_APP_API_URL}/products/by/search`;
 export const getDepartmentsApi = `${process.env.REACT_APP_API_URL}/departments`;
-export const productsEndPointAPi =  `${process.env.REACT_APP_API_URL}/products`;
+export const productsEndPointApi =  `${process.env.REACT_APP_API_URL}/products`;
+export const productEndPointApi = `${process.env.REACT_APP_API_URL}/product`;
