eb3f501e989d0ddc713db7bbb02ad623a2aefad8
Abubakar <Haywayaheadshotstrategy.fx@gmail.com>
2023-09-26 08:16:39 -0500

    consolidate products end point to adhere to DRY

--- Qfront/src/redux/api.js
+++ Qfront/src/redux/api.js
@@ -1,6 +1,4 @@
-export const getProductsByPriceApi = `${process.env.REACT_APP_API_URL}/products?sortBy=price&order=asc&limit=10`;
 export const getAllCategoriesApi = `${process.env.REACT_APP_API_URL}/categories`;
-export const loadBySellApi = `${process.env.REACT_APP_API_URL}/products?sortBy=sold&order=desc&limit=10`;
-export const getNewArrivalsApi = `${process.env.REACT_APP_API_URL}/products?sortBy=sold&order=desc&limit=10`;
 export const fetchFilteredProductsApi = `${process.env.REACT_APP_API_URL}/products/by/search`;
 export const getDepartmentsApi = `${process.env.REACT_APP_API_URL}/departments`;
+export const productsEndPointAPi =  `${process.env.REACT_APP_API_URL}/products`;
