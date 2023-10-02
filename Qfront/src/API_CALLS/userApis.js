108246e940b29df66b8cc9780b343d3fde5bf9a8
Abubakar <Haywayaheadshotstrategy.fx@gmail.com>
2023-10-02 10:22:33 -0500

    comment initiateTransaction API call

--- Qfront/src/API_CALLS/userApis.js
+++ Qfront/src/API_CALLS/userApis.js
@@ -1,49 +1,35 @@
-// export const getTransactionFees = (cost) => {
-//   let fee = { amount: cost };
-//   return fetch(`${process.env.REACT_APP_API_URL}/rave/transaction/fees`, {
+// export const initateTransaction = (
+  // ref,
+  // mail,
+  // cost,
+  // kind,
+  // number,
+  // telco,
+  // redirect,
+  // name
+// ) => {
+//   let payment = {
+    // currency: "GHS",
+    // fullname: name,
+    // tx_ref: ref,
+    // amount: cost,
+    // email: mail,
+    // type: kind,
+    // phone_number: number,
+    // network: telco,
+    // redirect_url: redirect,
+//   };
+//   return fetch(`${process.env.REACT_APP_API_URL}/rave/momopayment`, {
 //     method: "POST",
 //     headers: {
 //       Accept: "application/json",
 //       "Content-type": "application/json",
 //     },
-//     body: JSON.stringify(fee),
+//     body: JSON.stringify(payment),
 //   })
 //     .then((res) => res.json())
 //     .catch((err) => console.log(err));
 // };
-
-export const initateTransaction = (
-  ref,
-  mail,
-  cost,
-  kind,
-  number,
-  telco,
-  redirect,
-  name
-) => {
-  let payment = {
-    currency: "GHS",
-    fullname: name,
-    tx_ref: ref,
-    amount: cost,
-    email: mail,
-    type: kind,
-    phone_number: number,
-    network: telco,
-    redirect_url: redirect,
-  };
-  return fetch(`${process.env.REACT_APP_API_URL}/rave/momopayment`, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-type": "application/json",
-    },
-    body: JSON.stringify(payment),
-  })
-    .then((res) => res.json())
-    .catch((err) => console.log(err));
-};
 
 export const createAuthOrder = (userId, access, orderData) => {
   return fetch(`${process.env.REACT_APP_API_URL}/order/create/${userId}`, {
