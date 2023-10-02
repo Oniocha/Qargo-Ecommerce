95c5d751b1b24babfabb73d591e719907ffd1207
Abubakar <Haywayaheadshotstrategy.fx@gmail.com>
2023-10-02 10:23:30 -0500

    and transactionEndPointApi

--- Qfront/src/redux/api.js
+++ Qfront/src/redux/api.js
@@ -2,4 +2,4 @@ export const getAllCategoriesApi = `${process.env.REACT_APP_API_URL}/categories`
 export const getDepartmentsApi = `${process.env.REACT_APP_API_URL}/departments`;
 export const productsEndPointApi =  `${process.env.REACT_APP_API_URL}/products`;
 export const productEndPointApi = `${process.env.REACT_APP_API_URL}/product`;
-export const transactionsEndPointApi = `${process.env.REACT_APP_API_URL}/rave/transaction`
+export const transactionsEndPointApi = `${process.env.REACT_APP_API_URL}/rave`
