e10a32a93b820c5c8f88a08c13d7a67996340e06
Abubakar <Haywayaheadshotstrategy.fx@gmail.com>
2023-10-05 09:17:15 -0500

    add ordersEndPoint Api

--- Qfront/src/redux/api.js
+++ Qfront/src/redux/api.js
@@ -2,4 +2,5 @@ export const getAllCategoriesApi = `${process.env.REACT_APP_API_URL}/categories`
 export const getDepartmentsApi = `${process.env.REACT_APP_API_URL}/departments`;
 export const productsEndPointApi =  `${process.env.REACT_APP_API_URL}/products`;
 export const productEndPointApi = `${process.env.REACT_APP_API_URL}/product`;
-export const transactionsEndPointApi = `${process.env.REACT_APP_API_URL}/rave`
+export const transactionsEndPointApi = `${process.env.REACT_APP_API_URL}/rave`;
+export const ordersEndPoint = `${process.env.REACT_APP_API_URL}/order`
